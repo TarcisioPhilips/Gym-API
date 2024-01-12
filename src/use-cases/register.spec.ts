a621f96e24e63c775d9d29fa723ae24574b77eed
Tarcisio Philips <tarcisiogabriel.philips@gmail.com>
2024-01-12 13:09:56 -0300

    test: hash password upon registration

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,5 +1,35 @@
-import { expect, test} from 'vitest'
+import { expect, describe, it} from 'vitest'
+import { RegisterUseCase } from './register'
+import { compare } from 'bcryptjs'
 
-test('check if it works', () => { 
-    expect(2+2).toBe(4) 
+
+
+describe('Register User Case', () =>{
+    it('should hash user password upon registration', async () => {
+        const registerUserCase = new RegisterUseCase({
+            async findByEmail(email){
+                return null
+            },
+            
+            async create(data) {
+                return {
+                    id: 'user-1',
+                    name: data.name,
+                    email: data.email,
+                    password_hash: data.password_hash,
+                    created_at: new Date()
+                }
+            },
+        })
+
+        const {user} = await registerUserCase.execute({
+            name: 'John Doe',
+            email:'jonhdoe@example.com',
+            password: '123456'
+        })
+
+        const isPasswordCorrectHashed = await compare('123456',user.password_hash)
+
+        expect(isPasswordCorrectHashed).toBe(true)
+    })
 })
