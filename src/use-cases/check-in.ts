39a476b6bffb258a8d65c0da345acf153a7174be
Tarcisio Philips <tarcisiogabriel.philips@gmail.com>
2024-01-19 16:53:23 -0300

    feat: latitude and longitude calc distance

--- src/use-cases/check-in.ts
+++ src/use-cases/check-in.ts
@@ -2,7 +2,7 @@ import { CheckIn } from "@prisma/client";
 import { CheckInsRepository } from "@/repositories/check-ins-repository";
 import { GymsRepository } from "@/repositories/gyms-repository";
 import { ResourceNotFoundError } from "./errors/resource-not-found-error";
-
+import { getDistanceBetweenCoordinates } from "@/utils/get-distance-between-coordinates";
 interface CheckInUseCaseRequest {
   userId: string;
   gymId: string;
@@ -11,38 +11,54 @@ interface CheckInUseCaseRequest {
 }
 
 interface CheckInUseCaseResponse {
-  checkIn: CheckIn
+  checkIn: CheckIn;
 }
 
 export class CheckInUseCase {
   constructor(
     private checkInsRepository: CheckInsRepository,
     private gymsRepository: GymsRepository
-    ) {}
+  ) {}
 
   async execute({
     userId,
     gymId,
+    userLatitude,
+    userLongitude,
   }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
-    const gym = await this.gymsRepository.findById(gymId)
+    const gym = await this.gymsRepository.findById(gymId);
 
-    if(!gym){
-      throw new ResourceNotFoundError()
+    if (!gym) {
+      throw new ResourceNotFoundError();
     }
 
-    const checkInOnSameDay = await this.checkInsRepository.findByUserIdOnDate(userId, new Date())
-    
-    if(checkInOnSameDay){
-      throw new Error
+    const distance = getDistanceBetweenCoordinates(
+      { latitude: userLatitude, longitude: userLongitude },
+      { latitude: gym.latitude.toNumber(), longitude: gym.longitude.toNumber() }
+    );
+
+    const MAX_DISTANCE_IN_KILOMETERS = 0.1;
+
+    if (distance > MAX_DISTANCE_IN_KILOMETERS) {
+      throw new Error;
+    }
+
+    const checkInOnSameDay = await this.checkInsRepository.findByUserIdOnDate(
+      userId,
+      new Date()
+    );
+
+    if (checkInOnSameDay) {
+      throw new Error;
     }
 
     const checkIn = await this.checkInsRepository.create({
-        gym_id: gymId,
-        user_id:userId
-    })
-    
+      gym_id: gymId,
+      user_id: userId,
+    });
+
     return {
-        checkIn,
-    }
+      checkIn,
+    };
   }
 }
