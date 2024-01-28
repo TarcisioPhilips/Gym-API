6e02ce0230c492c77d6d8b108851d72e2687aab0
Tarcisio Philips <tarcisiogabriel.philips@gmail.com>
2024-01-27 21:26:28 -0300

    feat: search gyms use case

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -4,7 +4,7 @@ import { randomUUID } from "crypto";
 
 export class InMemoryGymsRepository implements GymsRepository {
     public items: Gym[] = []
-
+    
     async findById(id: string) {
         const gym = this.items.find((item) => item.id === id)
         
@@ -28,6 +28,12 @@ export class InMemoryGymsRepository implements GymsRepository {
         
         this.items.push(gym)
         return gym
+    }
+
+    async searchMany(query: string, page: number){
+        return this.items
+            .filter((item)=>item.title.includes(query))
+            .slice((page-1)*20,page*20)
     }
 
 }
