9120f9f73587599cd69b9e0910d383abbeb8888a
Tarcisio Philips <tarcisiogabriel.philips@gmail.com>
2024-01-29 16:45:28 -0300

    test: late check in validation

--- src/use-cases/validate-check-in.spec.ts
+++ src/use-cases/validate-check-in.spec.ts
@@ -11,11 +11,11 @@ describe("Validade Check-in Use Case", () => {
     checkInsRepository = new InMemoryCheckInsRepository();
     sut = new ValidateCheckInUseCase(checkInsRepository);
 
-    // vi.useFakeTimers();
+    vi.useFakeTimers();
   });
 
   afterEach(() => {
-    // vi.useRealTimers();
+    vi.useRealTimers();
   });
 
   it("should be able to validate the check-in", async () => {
@@ -33,10 +33,28 @@ describe("Validade Check-in Use Case", () => {
   });
 
   it("should not be able to validate an inexistent check-in", async () => {
-    await expect(() => 
+    await expect(() =>
       sut.execute({
         checkInId: "inexistent-check-in-id",
-      }),
+      })
     ).rejects.toBeInstanceOf(ResourceNotFoundError);
+  });
+
+  it("should not be able to validate the check-in after 20 minutes of its creation", async () => {
+    vi.setSystemTime(new Date(2023, 0, 1, 13, 40)); // utc
+
+    const createdcheckIn = await checkInsRepository.create({
+      gym_id: "gym-01",
+      user_id: "user-01",
+    });
+
+    const twentyOneMinutesInMS = 1000* 60 * 21
+
+    vi.advanceTimersByTime(twentyOneMinutesInMS)
+
+    await expect(() => sut.execute({
+        checkInId: createdcheckIn.id,
+      })).rejects.toBeInstanceOf(Error)
+
   });
 });
