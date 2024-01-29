b4bb24d92885996922cb57c2193aa1ca8df21c9f
Tarcisio Philips <tarcisiogabriel.philips@gmail.com>
2024-01-29 16:31:10 -0300

    feat: validate check in use case

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -4,53 +4,66 @@ import { randomUUID } from "node:crypto";
 import dayjs from "dayjs";
 
 export class InMemoryCheckInsRepository implements CheckInsRepository {
-    public items: CheckIn[] = []
-    
-    async findByUserIdOnDate(userId: string, date: Date) {
-        const startOfDay = dayjs(date).startOf('date')
-        const endOfDay = dayjs(date).endOf('date')
+  public items: CheckIn[] = [];
 
+  async findById(id: string) {
+    const checkIn = this.items.find((item) => item.id === id);
 
-        const checkInOnSameDate = this.items.find(checkIn => {
-            const checkInDate = dayjs(checkIn.created_at)
-            const isOnSameDate = 
-                checkInDate.isAfter(startOfDay) && checkInDate.isBefore(endOfDay)
-            
-            return checkIn.user_id === userId && isOnSameDate 
-        })
+    if (!checkIn) {
+      return null;
+    }
+    return checkIn;
+  }
 
-        if(!checkInOnSameDate) {
-            return null
-        }
+  async findByUserIdOnDate(userId: string, date: Date) {
+    const startOfDay = dayjs(date).startOf("date");
+    const endOfDay = dayjs(date).endOf("date");
 
-        return checkInOnSameDate
+    const checkInOnSameDate = this.items.find((checkIn) => {
+      const checkInDate = dayjs(checkIn.created_at);
+      const isOnSameDate =
+        checkInDate.isAfter(startOfDay) && checkInDate.isBefore(endOfDay);
+
+      return checkIn.user_id === userId && isOnSameDate;
+    });
+
+    if (!checkInOnSameDate) {
+      return null;
     }
 
-    async findManyByUserId(userId: string, page:number){
-       return this.items
-        .filter(item => item.user_id ===userId)
-        .slice((page-1)*20, page*20)
-    }
+    return checkInOnSameDate;
+  }
 
-    async countByUserId(userId: string) {
-        return this.items
-        .filter(item => item.user_id ===userId)
-        .length
-    }
+  async findManyByUserId(userId: string, page: number) {
+    return this.items
+      .filter((item) => item.user_id === userId)
+      .slice((page - 1) * 20, page * 20);
+  }
 
-    async create(data: Prisma.CheckInUncheckedCreateInput) {
-        const checkIn = {
-        id: randomUUID(),
-        user_id: data.user_id,
-        gym_id: data.gym_id,
-        validated_at: data.validated_at ? new Date(data.validated_at) : null,
-        created_at: new Date()
-        }
-        
-        this.items.push(checkIn)
-        
-        return checkIn
-    }
+  async countByUserId(userId: string) {
+    return this.items.filter((item) => item.user_id === userId).length;
+  }
 
+  async create(data: Prisma.CheckInUncheckedCreateInput) {
+    const checkIn = {
+      id: randomUUID(),
+      user_id: data.user_id,
+      gym_id: data.gym_id,
+      validated_at: data.validated_at ? new Date(data.validated_at) : null,
+      created_at: new Date(),
+    };
+
+    this.items.push(checkIn);
+
+    return checkIn;
+  }
+
+  async save(checkIn: CheckIn) {
+    const checkInIndex = this.items.findIndex((item) => item.id === checkIn.id)
+
+    if(checkInIndex >= 0) {
+        this.items[checkInIndex] = checkIn
+    }
+    return checkIn
+  }
 }
-
