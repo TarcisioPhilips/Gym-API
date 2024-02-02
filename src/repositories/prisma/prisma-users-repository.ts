df33a81adc2046dae29d73299a126372560d7c32
Tarcisio Philips <tarcisiogabriel.philips@gmail.com>
2024-02-02 09:37:53 -0300

    feat: prisma user repository

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -1,31 +1,33 @@
 import { prisma } from "@/lib/prisma";
-import {Prisma, User} from '@prisma/client'
+import { Prisma, User } from "@prisma/client";
 import { UsersRepository } from "../users-repository";
 
+export class PrismaUsersRepository implements UsersRepository {
+  async findById(id: string): Promise<User | null> {
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
 
-export class PrismaUsersRepository implements UsersRepository{
-    findById(id: string): Promise<User | null> {
-        throw new Error("Method not implemented.");
-    }
-    
+    return user;
+  }
 
-    async findByEmail(email: string) {
-        const user = await prisma.user.findUnique({
-            where: {
-                email
-            }
-        })
-        
-        return user
-    }
+  async findByEmail(email: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
 
-    async create(data: Prisma.UserCreateInput) {
-        const user = await prisma.user.create({
-            data
-        })
+    return user;
+  }
 
-        return user
-    }
-    
+  async create(data: Prisma.UserCreateInput) {
+    const user = await prisma.user.create({
+      data,
+    });
+
+    return user;
+  }
 }
-
